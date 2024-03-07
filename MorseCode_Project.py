1eec1ec4a01a488f4c89e64ae3cc450274def726
StephenRuth1024 <162595833+StephenRuth1024@users.noreply.github.com>
2024-03-07 22:12:57 +0000

    Add files via upload

---
+++ MorseCode_Project.py
@@ -0,0 +1,39 @@
+
+
+
+morse_code_dict = {
+
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': '       '
+
+}
+
+
+
+
+def morse_code_converter():
+    string = input('Enter Text:' )
+    formatted_string = string.upper()
+    morse_code_full = ''
+    for i in formatted_string:
+        morse_code_element = morse_code_dict[i]
+        morse_code_full = morse_code_full + morse_code_element + '   '
+    print(f'{string} in morse code = {morse_code_full}')
+    return morse_code_full
+
+
+
+
+
+morse_code_converter()
+
+
+
+
+
+
